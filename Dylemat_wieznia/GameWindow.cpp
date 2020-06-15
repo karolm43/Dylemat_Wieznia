e363d86fad00c66443fc9f275ad75b2b502ab1bc
Karol <karol.marc43@gmail.com>
2020-06-15 13:23:31 +0200

    dwa razy main

--- Dylemat_wieznia/GameWindow.cpp
+++ Dylemat_wieznia/GameWindow.cpp
@@ -1,14 +1,1 @@
 
-#include "GameWindow.h"
-
-using namespace System;
-using namespace Dylematwieznia;
-[STAThreadAttribute]
-
-int main(array <System::String^>^ args)
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew GameWindow());
-	return 0;
-}
