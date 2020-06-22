de4a1de8d23a60d3deb3fa47d0891648cd284512
Geggu <geggupl@gmail.com>
2020-06-23 00:03:21 +0200

    Poprawa puntacji

--- Dylemat_wieznia/Decision.cpp
+++ Dylemat_wieznia/Decision.cpp
@@ -5,15 +5,15 @@ void Decision::setDecision(bool decision)
 	this->decision = decision;
 }
 int Decision::operator==(const Decision& d) {
-	if (d.decision == this->decision && this->decision) {
+	if (d.decision && this->decision) {
 		return 2;
 	}
 	else if (d.decision == this->decision) {
 		return 6;
 	}
 	else if (d.decision) {
-		return 0;
+		return 10;
 	} else {
-		return 2;
+		return 0;
 	}
 }
